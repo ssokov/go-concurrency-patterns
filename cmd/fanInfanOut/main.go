81f74887abe12fde1ff5d657b11cdb0f3c1e5f05
ssokov <sokovykh.work@gmail.com>
2026-02-23 16:24:46 +0300

    fear(fanin\fanout) add fanin\fanout

---
+++ cmd/fanInfanOut/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+
+	"github.com/ssokov/go-concurrency-patterns/internal/fanin"
+	"github.com/ssokov/go-concurrency-patterns/internal/fanout"
+	"github.com/ssokov/go-concurrency-patterns/internal/generator"
+)
+
+func worker[T any, R any](ctx context.Context, in <-chan T, count int, fn func(T) R) <-chan R {
+	out := make(chan R)
+
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-ctx.Done():
+					return
+				case out <- fn(val):
+				}
+			}
+		}
+	}()
+
+	return out
+}
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	input := generator.Generate(ctx, data, 1)
+
+	// fan-out на N каналов
+	N := 3
+	outs := fanout.FanOut(ctx, input, N)
+
+	// запускаем N worker'ов
+	workerOuts := make([]<-chan int, 0, N)
+
+	for _, ch := range outs {
+		workerOuts = append(workerOuts,
+			worker(ctx, ch, 0, func(v int) int {
+				return v * v
+			}),
+		)
+	}
+
+	// fan-in
+	result := fanin.FanIn(ctx, 0, workerOuts...)
+
+	for v := range result {
+		println(v)
+	}
+}
